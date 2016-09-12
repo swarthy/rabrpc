6922e45899ad289613c193f99f69400409a145d5
Alexander <horroshow@mail.ru>
2016-09-12 12:18:42 +0500

    more tests

--- test/unit/lib/transformConfig/req-res/parseExchange.test.js
+++ test/unit/lib/transformConfig/req-res/parseExchange.test.js
@@ -2,14 +2,17 @@ const parseExchange = require('../../../../../lib/transformConfig/req-res/parseE
 
 describe('transformConfig req-res parseExchange', () => {
   it('should return exchange config without merging options', () => {
-    const config = parseExchange({serviceName: 'test', autoDelete: true})
+    const config = parseExchange({serviceName: 'test', autoDelete: true, replyTimeout: 1234})
     expect(config.name).to.be.eql('req-res.test')
     expect(config.type).to.be.eql('direct')
+    expect(config.autoDelete).to.not.be.true
+    expect(config.replyTimeout).to.not.be.eql(1234)
   })
   it('should return merged exchange config if second argument is true', () => {
-    const config = parseExchange({serviceName: 'test', autoDelete: true}, true)
+    const config = parseExchange({serviceName: 'test', autoDelete: true, replyTimeout: 1234}, true)
     expect(config.name).to.be.eql('req-res.test')
     expect(config.type).to.be.eql('direct')
-    expect(config.autoDelete).to.be.eql(true)
+    expect(config.autoDelete).to.be.true
+    expect(config.replyTimeout).to.be.eql(1234)
   })
 })
